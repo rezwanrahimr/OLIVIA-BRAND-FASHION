40de7c9cd45fd9cc12b4f1d4cf514e51cffd933b
rezwanrahimr <rezwanrahim69@gmail.com>
2022-08-26 19:05:09 +0600

    signup page complite

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -39,37 +39,6 @@ const SignUp = () => {
                             </a>
                         </div>
                     </form>
-                    <form action="#" class="formControl sign-up-form">
-                        <h2 class="title">Sign up</h2>
-                        <div class="input-field">
-                            <i class="fas fa-user"></i>
-                            <input type="text" placeholder="Username" />
-                        </div>
-                        <div class="input-field">
-                            <i class="fas fa-envelope"></i>
-                            <input type="email" placeholder="Email" />
-                        </div>
-                        <div class="input-field">
-                            <i class="fas fa-lock"></i>
-                            <input type="password" placeholder="Password" />
-                        </div>
-                        <input type="submit" class="btn" value="Sign up" />
-                        <p class="social-text">Or Sign up with social platforms</p>
-                        <div class="social-media">
-                            <a href="#" class="social-icon">
-                                <i class="fab fa-facebook-f"></i>
-                            </a>
-                            <a href="#" class="social-icon">
-                                <i class="fab fa-twitter"></i>
-                            </a>
-                            <a href="#" class="social-icon">
-                                <i class="fab fa-google"></i>
-                            </a>
-                            <a href="#" class="social-icon">
-                                <i class="fab fa-linkedin-in"></i>
-                            </a>
-                        </div>
-                    </form>
                 </div>
             </div>
 
@@ -83,19 +52,6 @@ const SignUp = () => {
                         <Link to='/Login'><Button class="LoginButtontransparent" id="sign-up-btn" variant="primary">SIGN-IN</Button>{' '}</Link>
                     </div>
                     <img src={registarImg} class="image" alt="" />
-                </div>
-                <div class="panel right-panel">
-                    <div class="content">
-                        <h3>One of us ?</h3>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-                            laboriosam ad deleniti.
-                        </p>
-                        <button class="LoginButtontransparent" id="sign-in-btn">
-                            Sign in
-                        </button>
-                    </div>
-                    <img src="img/register.svg" class="image" alt="" />
                 </div>
             </div>
         </div>
