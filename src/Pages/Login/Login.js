f4959207f19876140384f70c11a6792fa845c0d6
rezwanrahimr <rezwanrahim69@gmail.com>
2022-08-26 17:46:29 +0600

    login complite

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
                             <i class="fas fa-lock"></i>
                             <input type="password" placeholder="Password" />
                         </div>
-                        <Button className='px-5' variant="outline-primary">Login</Button>{' '}
+                        <Button className='px-5' variant="outline-primary">LOGIN</Button>{' '}
                         <p class="social-text">Or Sign in with social platforms</p>
                         <div class="social-media">
                             <a href="#" class="social-icon">
@@ -45,11 +45,11 @@ const Login = () => {
             <div class="panels-container">
                 <div class="panel left-panel">
                     <div class="content">
-                        <h3></h3>
+                        <h3>NOT MEMBER? BECOME ON TODAY</h3>
                         <p>
-                            
+                        CREATE A NEW ACCOUNT. ONCE YOU VE SET IT UP YOU CAN TAKE ADVANTAGE OF MANY BENEFITS OF MEMBERSHIP.
                         </p>
-                        <Link to='/signUP'><Button  class="LoginButtontransparent" id="sign-up-btn" variant="primary">Sign-up</Button>{' '}</Link>
+                        <Link to='/signUP'><Button  class="LoginButtontransparent" id="sign-up-btn" variant="primary">SIGN-UP</Button>{' '}</Link>
                     </div>
                     <img src={IMG} class="image" alt="" />
                 </div>
