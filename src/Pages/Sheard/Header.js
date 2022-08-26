46b9738497cfc001862d7737c335682e7ebe4ff1
rezwanrahimr <rezwanrahim69@gmail.com>
2022-08-26 19:42:50 +0600

    some design add

--- src/Pages/Sheard/Header.js
+++ src/Pages/Sheard/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { NavDropdown } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 import CompanyLogo from '../../images/companyLogo.png';
 
 const Header = () => {
@@ -15,16 +17,25 @@ const Header = () => {
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
               <Nav
-                className="me-auto my-2 my-lg-0"
+                className="me-auto my-2 my-lg-0 "
                 style={{ maxHeight: '100px' }}
                 navbarScroll
               >
-                <Nav.Link href="/home" className='fw-bold text-black'>HOME</Nav.Link>
+                <Link className='text-decoration-none' to="/home"><Nav.Link href="/home" className='fw-bold text-black'>HOME</Nav.Link></Link>
                 <Nav.Link href="#action2" className='fw-bold text-black'>COLLECTION </Nav.Link>
                 <Nav.Link href="#action2" className='fw-bold text-black'>SHOP </Nav.Link>
                 <Nav.Link href="#action2" className='fw-bold text-black'>ABOUT </Nav.Link>
                 <Nav.Link href="#action2" className='fw-bold text-black'>PAGES </Nav.Link>
-                
+                <NavDropdown
+              id="nav-dropdown-white-example"
+              title="USER"
+              menuVariant="white"
+              className='fw-bold text-black'
+            >
+              <Link className='text-decoration-none' to="/Login"><NavDropdown.Item href="/Login">LOGIN</NavDropdown.Item></Link>
+              <Link className='text-decoration-none' to="/SignUp"><NavDropdown.Item  href="/SignUp">REGISTER</NavDropdown.Item></Link>
+             
+            </NavDropdown>
               </Nav>
               <Form className="d-flex">
                 <Form.Control
