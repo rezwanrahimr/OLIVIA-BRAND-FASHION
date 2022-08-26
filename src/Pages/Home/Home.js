7fa3297b5913f0797e51f9777f7e9ca59ecc79b3
rezwanrahimr <rezwanrahim69@gmail.com>
2022-08-26 21:19:31 +0600

    another section done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import WomenMen from '../WomenMen/WomenMen';
 import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <WomenMen></WomenMen>
         </div>
     );
 };
