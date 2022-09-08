714db81db9fd1c9936eefd747254011920850ea7
rezwanrahimr <rezwanrahim69@gmail.com>
2022-09-08 12:56:11 +0600

    show product in my order page

--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -1,65 +1,85 @@
 import { MDBBadge, MDBBtn, MDBTable, MDBTableBody, MDBTableHead } from 'mdb-react-ui-kit';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import Loading from '../Sheard/Loading';
 
 const MyOrder = () => {
-    return (
-        <div>
-            <h1 className='text-center'>MY ORDER</h1>
-            <div>
-            <MDBTable align='middle'>
-      <MDBTableHead>
-        <tr>
-          <th scope='col'>PRODUCTS</th>
-          <th scope='col'>TRANSACTION ID</th>
-          <th scope='col'>QUANTITY</th>
-          <th scope='col'>PRICE	</th>
-          <th scope='col'>PAY MENT</th>
-          <th scope='col'>CANCLE</th>
-        </tr>
-      </MDBTableHead>
-      <MDBTableBody>
-        <tr>
-          <td>
-            <div className='d-flex align-items-center'>
-              <img
-                src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                alt=''
-                style={{ width: '45px', height: '45px' }}
-                className='rounded-circle'
-              />
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>John Doe</p>
-                <p className='text-muted mb-0'>john.doe@gmail.com</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>Software engineer</p>
-            <p className='text-muted mb-0'>IT department</p>
-          </td>
-          <td>
-            <MDBBadge color='success' pill>
-              Active
-            </MDBBadge>
-          </td>
-          <td>Senior</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-        </tr>
-        
-      </MDBTableBody>
-    </MDBTable>
-            </div>
-        </div>
-    );
+  const [products, setProducts] = useState([]);
+  const [user, loading] = useAuthState(auth);
+  const email = user?.email;
+  useEffect(() => {
+    fetch(`http://localhost:5000/order/${email}`)
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data)
+      })
+  }, [user])
+
+  if (loading) {
+    return <Loading></Loading>
+  }
+  console.log(products)
+  return (
+    <div>
+      <h1 className='text-center'>MY ORDER</h1>
+      <div>
+        <MDBTable align='middle'>
+          <MDBTableHead>
+            <tr>
+              <th scope='col'>PRODUCTS</th>
+              <th scope='col'>TRANSACTION ID</th>
+              <th scope='col'>QUANTITY</th>
+              <th scope='col'>PRICE	</th>
+              <th scope='col'>PAY MENT</th>
+              <th scope='col'>CANCLE</th>
+            </tr>
+          </MDBTableHead>
+          {
+            products.map(product => <MDBTableBody key={product._id}>
+              <tr>
+                <td>
+                  <div className='d-flex align-items-center'>
+                    <img
+                      src={product.CartProductImage}
+                      alt=''
+                      style={{ width: '45px', height: '45px' }}
+                      className='rounded-circle'
+                    />
+                    <div className='ms-3'>
+                      <p className='fw-bold mb-1'>{product.CartProductName}</p>
+                      <p className='text-muted mb-0'>{product.userName}</p>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <p className='fw-normal mb-1'>Software engineer</p>
+                  <p className='text-muted mb-0'>IT department</p>
+                </td>
+                <td>
+                  <MDBBadge color='success' pill>
+                    {product.productQuantity}
+                  </MDBBadge>
+                </td>
+                <td>{product.CartProductPrice}</td>
+                <td>
+                  <MDBBtn color='link' rounded size='sm'>
+                    Edit
+                  </MDBBtn>
+                </td>
+                <td>
+                  <MDBBtn color='link' rounded size='sm'>
+                    Edit
+                  </MDBBtn>
+                </td>
+              </tr>
+
+            </MDBTableBody>)
+          }
+        </MDBTable>
+      </div>
+    </div>
+  );
 };
 
 export default MyOrder;
